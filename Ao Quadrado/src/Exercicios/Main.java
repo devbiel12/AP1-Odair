b55b0799fdab388dd6ad9c5142b49e65e9860320
Deziderio55 <mateusdeziderio28@gmail.com>
2025-03-30 15:52:13 -0300

    Add files via upload

---
+++ Ao Quadrado/src/Exercicios/Main.java
@@ -0,0 +1,18 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um numero: ");
+        float numero = sc.nextFloat();
+        sc.close();
+
+        float quadrado = numero * numero;
+        float cubo = numero * numero * numero;
+
+        System.out.println("O " + numero + " ao quadrado é: " + quadrado + " e o cubo desse numero é: " + cubo);
+    }
+}
