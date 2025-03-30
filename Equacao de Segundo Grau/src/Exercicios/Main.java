b3139d1136c2d362f423cfb3b63992a16bde65b0
Deziderio55 <mateusdeziderio28@gmail.com>
2025-03-30 15:54:10 -0300

    Add files via upload

---
+++ Equacao de Segundo Grau/src/Exercicios/Main.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o valor de 'a' da formula de 2º grau: ");
+        double valorA = sc.nextFloat();
+
+        System.out.println("Digite o valor de 'b' da formula de 2ºgrau: ");
+        double valorB = sc.nextFloat();
+
+        System.out.println("Digite o valor de 'c' da formula de 2ºgrau: ");
+        double valorC = sc.nextFloat();
+        sc.close();
+
+        double delta = (valorB*valorB) - 4*valorA*valorC;
+        double xPositivo = (-valorB + java.lang.Math.sqrt(delta))/ (2 * valorA);
+        double xNegativo = (-valorB - java.lang.Math.sqrt(delta))/ (2 * valorA);
+
+
+        System.out.println("O valor de delta é: " + delta + " e os valores de X da equação de segundo grau são: "+xPositivo+" e "+xNegativo);
+    }
+}
