eefaf286c01ed3e95767761b4f65762130ca2e94
Deziderio55 <mateusdeziderio28@gmail.com>
2025-03-30 16:11:11 -0300

    Add files via upload

---
+++ Celsius para Fahrenheit/src/Exercicios/Main.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um grau em Celsius: ");
+        double celsius = sc.nextInt();
+        sc.close();
+
+        double fahrenheit = celsius * 1.8 + 32;
+
+        System.out.println("A temperatura de "+celsius+"ºC em Fahrenheit é "+ fahrenheit+"°F");
+
+    }
+}
